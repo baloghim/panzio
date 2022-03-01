be4f873d4128e9ffb0173532bfae0e21a97be2ed
baloghim <balogh.imre.mt@gmail.com>
2022-03-01 10:57:23 +0100

    szoba tipusa

--- program.js
+++ program.js
@@ -1,5 +1,8 @@
 function kalkulacio()
 {
+
+    // Foglalt napok száma
+
  var Erk = document.getElementById("Erkezes").value;
  var Tav = document.getElementById("Tavozas").value;
 
@@ -10,5 +13,39 @@ function kalkulacio()
 
  var NapokSzama = ElteltIdo / ( 1000 * 3600 * 24 );
 
- alert(NapokSzama);
+ // alert(NapokSzama);
+
+
+    // Szoba típusa
+
+
+    var szobaAr=0;
+
+    if(document.getElementById("egyAgy").checked==true)
+    {
+        szobaAr=9000;
+    }
+
+
+    if(document.getElementById("ketAgy").checked==true)
+    {
+        szobaAr=15000;
+    }
+
+
+    if(document.getElementById("ketAgy1p").checked==true)
+    {
+        szobaAr=18000;
+    }
+
+
+    if(document.getElementById("ketAgy2p").checked==true)
+    {
+        szobaAr=21000;
+    }
+
+
+  // Vendégek száma
+
+
 }
