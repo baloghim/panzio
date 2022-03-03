0734bca3facb0d7ee886278e2a350c976543b6c7
baloghim <balogh.imre.mt@gmail.com>
2022-03-03 12:36:54 +0100

    Peti kész

--- program.js
+++ program.js
@@ -21,25 +21,30 @@ var NapokSzama = Elteltido /( 1000 * 3600 * 24 );
     // Szoba típusa
 
     var SzobarAr=0;
+    var SzobaTipus = "";
 
     if(document.getElementById("EgyAgy").checked==true)
     {
         SzobarAr=9000;
+        SzobaTipus = "Egyágyas";
     }
 
     if(document.getElementById("KetAgy").checked==true)
     {
         SzobarAr=15000;
+        SzobaTipus = "Kétágyas";
     }
 
     if(document.getElementById("KetAgy1p").checked==true)
     {
         SzobarAr=18000;
+        SzobaTipus = "Kétágyas 1 pótággyal";
     }
 
     if(document.getElementById("KetAgy2p").checked==true)
     {
         SzobarAr=21000;
+        SzobaTipus = "Kétágyas 2 pótággyal";
     }
 
     // Vendégek száma
@@ -63,49 +68,58 @@ var NapokSzama = Elteltido /( 1000 * 3600 * 24 );
     // Ellátás 
 
     var Ellatas=0;
+    var EllatasTipus = "";
 
     if(document.getElementById("reggeli").checked==true)
     {
         Ellatas=900;
+        EllatasTipus = "Reggeli";
     }
 
     if(document.getElementById("felpanzio").checked==true)
     {
         Ellatas=2900;
+        EllatasTipus = "Félpanzió";
     }
 
     if(document.getElementById("teljespanzio").checked==true)
     {
         Ellatas=4900;
+        EllatasTipus = "Teljespanzió";
     }
 
         // Fürdő szolgáltatások
 
         var furdo=0;
+        var furdoTipus = "";
 
         if(document.getElementById("belteri").checked==true)
         {
             furdo+=800;
+            furdoTipus += "Beltéri, ";
         }
     
         if(document.getElementById("kulteri").checked==true)
         {
             furdo+=800;
+            furdoTipus += "Kültéri, ";
         }
     
         if(document.getElementById("szauna").checked==true)
         {
             furdo+=800;
+            furdoTipus += "Szauna, ";
         }
 
         if(document.getElementById("teljes").checked==true)
         {
             furdo=2000;
+            furdoTipus = "Teljes";
         }
 
         // Egyéb
 
-        var egyebek =document.getElementById("egyeb").value;
+        var egyebek = document.getElementById("egyeb").value;
         
     
 
@@ -118,6 +132,17 @@ var NapokSzama = Elteltido /( 1000 * 3600 * 24 );
 
         //szobaár
 
-        document.getElementById("szobaar").innerHTML=SzobarAr + " Ft";
+        document.getElementById("szobaar").innerHTML = SzobarAr * NapokSzama + " Ft";
+        document.getElementById("szobatipus").innerHTML = SzobaTipus;
+        document.getElementById("szobaarosszesen").innerHTML = SzobarAr * NapokSzama + " Ft";
+        document.getElementById("vendegekszama").innerHTML = VendegekSzama + " fő";
+        document.getElementById("ellatastipus").innerHTML = EllatasTipus;
+        document.getElementById("ellatasegyfore").innerHTML = Ellatas + " Ft";
+        document.getElementById("ellatasosszesen").innerHTML = Ellatas * VendegekSzama * NapokSzama + " Ft";
+        document.getElementById("furdotipus").innerHTML = furdoTipus;
+        document.getElementById("furdoegyfore").innerHTML = furdo + " Ft";
+        document.getElementById("furdoosszesen").innerHTML = furdo * VendegekSzama * NapokSzama + " Ft";
 
-}
+        document.getElementById("lathatosag").style.display = "none";
+
+    }
